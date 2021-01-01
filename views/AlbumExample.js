f6e7f2872a5ca16d8f9671f139cc5d923d91668f
anyongjitiger <anyongjitiger@gmail.com>
2021-01-01 12:06:32 +0800

    删除多余代码

--- views/AlbumExample.js
+++ views/AlbumExample.js
@@ -1,9 +1,7 @@
 // eslint-disable-next-line prettier/prettier
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import ToastExample from '../ToastExample';
 import {
-  NativeModules,
   PermissionsAndroid,
   StyleSheet,
   ScrollView,
@@ -18,17 +16,11 @@ import {
 import CameraRoll from "@react-native-community/cameraroll";
 import * as Progress from 'react-native-progress';
 import { uploadImage, uploadCheck } from '../utils/upload';
-import { getData } from '../utils/global-storage';
-import GlobalVar from '../utils/global-var.js';
-const common_url = GlobalVar.common_url;
 
 export default function AlbumExample({ navigation }) {
-  const [photoList, setPhotoList] = useState([]);
   const [imageList, setImageList] = useState([]);
   const [imageCount, setImageCount] = useState(0);
   const [imageUploaded, setImageUploaded] = useState(0);
-  const [videoList, setVideoList] = useState([]);
-  const [remotePhotoList, setRemotePhotoList] = useState([]);
   const [granted, setGranted] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   useEffect(() => {
@@ -54,7 +46,7 @@ export default function AlbumExample({ navigation }) {
         },
       );
       if (_granted === PermissionsAndroid.RESULTS.GRANTED) {
-        sync();
+        getLocalPhotos();
         setGranted(true);
       } else {
         console.log('获取读写权限失败');
@@ -128,43 +120,7 @@ export default function AlbumExample({ navigation }) {
     );
   };
 
-  const getRemotePhotos = () => {
-    axios
-      .get(common_url + 'albums/all', {
-        headers: {
-          // 'Content-Type': 'multipart/form-data;',
-          // 'x-access-token': token
-        },
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          const dataArray = response.data.Data;
-          let tempAry = [];
-          for (let v of dataArray) {
-            tempAry.push(`${common_url}show//${v.FileName}`);
-          }
-          setRemotePhotoList(tempAry);
-        } else {
-          console.log(response);
-        }
-      })
-      .catch(function (error) {
-        console.log("getRemotePhotos: ", error);
-      })
-      .then(function () {
-        // always executed
-      });
-  };
-
   const getLocalPhotos = () => {
-    NativeModules.GetLocalPhotos.getAllPhotoInfo().then((res) => {
-      const resp = JSON.parse(res);
-      setPhotoList(resp);
-    }).catch((err) => {
-      console.log('err', err);
-    });
-  };
-  const getLocalPhotos2 = () => {
     CameraRoll.getPhotos({
       first: 40,
       assetType: 'All',
@@ -179,17 +135,6 @@ export default function AlbumExample({ navigation }) {
       });
   };
 
-  const getLocalVideos = () => {
-    NativeModules.GetLocalVideos.getAllVideoInfo()
-      .then((res) => {
-        const resp = JSON.parse(res);
-        setVideoList(resp);
-      })
-      .catch((err) => {
-        console.log('err', err);
-      });
-  };
-  let upd = 0;
   const checkExists = async (list) => {
     const _list = list.map((item) => {
       return {
@@ -199,11 +144,9 @@ export default function AlbumExample({ navigation }) {
     })
     try {
       const value = await uploadCheck('uploadCheck/', _list);
-      console.log(value);
       if (value.State == 1) {
-        console.log(value.Data);
         if (value.Msg === 'no data') {
-          return [];
+          return null;
         } else {
           return value.Data;
         }
@@ -213,74 +156,51 @@ export default function AlbumExample({ navigation }) {
       console.log(err);
     }
   };
+
   const onPressBackup = () => {
     setModalVisible(true);
     const imagesNotUploaded = [];
     checkExists(imageList).then((list) => {
-      imageList.forEach(item => {
-        let img = item.node.image;
-        console.log("list", list);
-        JSON.parse(list).forEach(element => {
-          if (element.FileName == img.filename && element.FileSize == img.fileSize) {
-            imagesNotUploaded.push(item);
-          }
-        })
-      });
-      console.log(imagesNotUploaded);
-      const promises = imagesNotUploaded.map((p) => {
-        return new Promise((resolve, reject) => {
-          uploadImage('upload/', p.node.image)
-            .then((r) => {
-              upd++;
-              setImageUploaded(upd);
-              resolve(r);
-            })
-            .catch((err) => {
-              reject(err);
-            });
+      if (list == null) {
+        setImageUploaded(imageCount);
+        setModalVisible(false);
+        ToastExample.show('已完成同步！', ToastExample.SHORT);
+      } else {
+        imageList.forEach(item => {
+          let img = item.node.image;
+          JSON.parse(list).forEach(element => {
+            if (element.FileName == img.filename && element.FileSize == img.fileSize) {
+              imagesNotUploaded.push(item);
+            }
+          })
         });
-      });
-      Promise.all(promises).then(() => {
-        setModalVisible(false);
-        ToastExample.show('同步成功！', ToastExample.SHORT);
-      }).catch(function (reason) {
-        if (reason.message === 'Network Error') {
-          console.log('网络连接失败！');
-          ToastExample.show('网络连接失败，请检查服务器连接！', ToastExample.LONG);
-        }
-        setModalVisible(false);
-      });
+        let upd = 0;
+        const promises = imagesNotUploaded.map((p) => {
+          return new Promise((resolve, reject) => {
+            uploadImage('upload/', p.node.image)
+              .then((r) => {
+                upd++;
+                setImageUploaded(upd);
+                resolve(r);
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+        Promise.all(promises).then(() => {
+          setModalVisible(false);
+          ToastExample.show('同步成功！', ToastExample.SHORT);
+        }).catch(function (reason) {
+          if (reason.message === 'Network Error') {
+            console.log('网络连接失败！');
+            ToastExample.show('网络连接失败，请检查服务器连接！', ToastExample.LONG);
+          }
+          setModalVisible(false);
+        });
+      }
     });
   };
-
-  const sync = () => {
-    // getLocalPhotos();
-    // getLocalVideos();
-    getLocalPhotos2();
-  };
-
-  const views = photoList.map((p, index) => {
-    return (
-      <View style={styles.photoView} key={index}>
-        <Image style={styles.photo} source={{ uri: p.path }} />
-      </View>
-    );
-  });
-  const remotePhotos = remotePhotoList.map((p, index) => {
-    return (
-      <View style={styles.photoView} key={index}>
-        <Image style={styles.photo} source={{ uri: p }} />
-      </View>
-    );
-  });
-  const VideoThumbs = videoList.map((p, index) => {
-    return (
-      <View style={styles.photoView} key={index}>
-        <Image style={styles.photo}
-          source={{ uri: `data:image/jpeg;base64,${p.thumbPath}` }} />
-      </View>
-    );
-  });
 
   const Images = imageList.map((p, index) => {
     return (
@@ -299,19 +219,12 @@ export default function AlbumExample({ navigation }) {
           disabled={!granted}
         />
         <Button
-          onPress={sync}
+          onPress={getLocalPhotos}
           title="同步相册"
         />
-        {/* <Button
-          title="Go to Login"
-          onPress={() => navigation.navigate('Login')}
-        /> */}
       </View>
       <ScrollView style={styles.scrollView}>
-        <View style={styles.photos}>{views}</View>
-        <View style={styles.videos}>{VideoThumbs}</View>
         <View style={styles.photos}>{Images}</View>
-        {/* <View style={styles.photos}>{remotePhotos}</View> */}
       </ScrollView>
 
       <Modal
@@ -343,14 +256,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     width: '100%',
     flexWrap: 'wrap',
-  },
-  videos: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    flexWrap: 'wrap',
-    backgroundColor: 'lightgreen',
   },
   photoView: {
     marginTop: 10,
